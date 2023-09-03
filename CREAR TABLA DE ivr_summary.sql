76b0596b8b62c4a51ca62f2aae21bac79bf242e4
miguel <34697@DESKTOP-N680KH9.com>
2023-09-03 02:11:49 +0200

    Eliminacion de comentarios y formateo del codigo

--- CREAR TABLA DE ivr_summary.sql
+++ CREAR TABLA DE ivr_summary.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE
   keepcoding.ivr_summary AS
-WITH doc
-   AS(
+WITH
+  doc AS(
   SELECT
     calls_ivr_id AS doc_ivr_id,
     document_identification AS doc_document_identification,
@@ -16,7 +16,6 @@ WITH doc
     ORDER BY
       module_sequece DESC,
       step_sequence DESC) = 1)
-
 SELECT
   ivr_detail.calls_ivr_id AS ivr_id,
   ivr_detail.calls_phone_number AS phone_number,
@@ -37,9 +36,7 @@ END
   ivr_detail.document_type,
   ivr_detail.document_identification,
   IFNULL(MAX(NULLIF(ivr_detail.customer_phone, 'NULL')), 'DESCONOCIDO') AS customer_phone,
-  -- ivr_detail.customer_phone,
   IFNULL(MAX(NULLIF(ivr_detail.billing_account_id, 'NULL')), 'DESCONOCIDO') AS billing_account_id,
-  -- ivr_detail.billing_account_id,
   ivr_detail.calls_steps_module AS steps_module,
   ivr_detail.calls_module_aggregation AS module_aggregation,
   MAX(
@@ -53,29 +50,41 @@ END
   IF
     (ivr_detail.step_name = 'CUSTOMERINFOBYDNI.TX'
       AND ivr_detail.step_description_error = "NULL",1,0)) AS info_by_dni_lg,
-    CASE
-      WHEN TIMESTAMP_DIFF(ivr_detail.calls_start_date, LAG(ivr_detail.calls_start_date) OVER (PARTITION BY ivr_detail.calls_phone_number ORDER BY ivr_detail.calls_start_date), HOUR) <= 24 THEN 1
-    ELSE
-    0
-  END
-    AS repeated_phone_24H,
-    CASE
-      WHEN TIMESTAMP_DIFF(ivr_detail.calls_start_date, LEAD(ivr_detail.calls_start_date) OVER (PARTITION BY ivr_detail.calls_phone_number ORDER BY ivr_detail.calls_start_date), HOUR) <= 24 THEN 1
-    ELSE
-    0
-  END
-    AS cause_recall_phone_24H
-  FROM
-    keepcoding.ivr_detail
-  LEFT JOIN
-    doc
-  ON
-    ivr_detail.calls_ivr_id = doc.doc_ivr_id
-  LEFT JOIN
-    keepcoding.ivr_detail recalls
-  ON
-    ivr_detail.calls_phone_number <> 'NULL'
-    AND ivr_detail.calls_phone_number = recalls.calls_phone_number
-    AND ivr_detail.calls_ivr_id <> recalls.calls_ivr_id
-  GROUP BY
-    ivr_id, phone_number, ivr_result, vdn_aggregation, start_date, end_date, total_duration, customer_segment, ivr_language, steps_module, module_aggregation, document_type, document_identification;
+  CASE
+    WHEN TIMESTAMP_DIFF(ivr_detail.calls_start_date, LAG(ivr_detail.calls_start_date) OVER (PARTITION BY ivr_detail.calls_phone_number ORDER BY ivr_detail.calls_start_date), HOUR) <= 24 THEN 1
+  ELSE
+  0
+END
+  AS repeated_phone_24H,
+  CASE
+    WHEN TIMESTAMP_DIFF(ivr_detail.calls_start_date, LEAD(ivr_detail.calls_start_date) OVER (PARTITION BY ivr_detail.calls_phone_number ORDER BY ivr_detail.calls_start_date), HOUR) <= 24 THEN 1
+  ELSE
+  0
+END
+  AS cause_recall_phone_24H
+FROM
+  keepcoding.ivr_detail
+LEFT JOIN
+  doc
+ON
+  ivr_detail.calls_ivr_id = doc.doc_ivr_id
+LEFT JOIN
+  keepcoding.ivr_detail recalls
+ON
+  ivr_detail.calls_phone_number <> 'NULL'
+  AND ivr_detail.calls_phone_number = recalls.calls_phone_number
+  AND ivr_detail.calls_ivr_id <> recalls.calls_ivr_id
+GROUP BY
+  ivr_id,
+  phone_number,
+  ivr_result,
+  vdn_aggregation,
+  start_date,
+  end_date,
+  total_duration,
+  customer_segment,
+  ivr_language,
+  steps_module,
+  module_aggregation,
+  document_type,
+  document_identification;
