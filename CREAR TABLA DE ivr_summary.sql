90e0b3cb50a29597d9c13e3eed21cfd30cf2272b
miguel <34697@DESKTOP-N680KH9.com>
2023-09-03 02:06:25 +0200

    Correccion del la sentencia para crear la tabla de ivr_summary

--- CREAR TABLA DE ivr_summary.sql
+++ CREAR TABLA DE ivr_summary.sql
@@ -1,53 +1,81 @@
 CREATE OR REPLACE TABLE
-  keepcoding.ivr_summary AS (
+  keepcoding.ivr_summary AS
+WITH doc
+   AS(
   SELECT
-    calls_ivr_id AS ivr_id,
-    calls_phone_number AS phone_number,
-    calls_ivr_result AS ivr_result,
-    CASE
-      WHEN calls_vdn_label LIKE "ATC%" THEN "FRONT"
-      WHEN calls_vdn_label LIKE "TECH%" THEN "TECH"
-      WHEN calls_vdn_label = "ABSORPTION" THEN "ABSORPTION"
-    ELSE
-    "RESTO"
-  END
-    AS vdn_aggregation,
-    calls_start_date AS start_date,
-    calls_end_date AS end_date,
-    calls_total_duration AS total_duration,
-    calls_customer_segment AS customer_segment,
-    calls_ivr_language AS ivr_language,
-    document_type,
-    document_identification,
-    customer_phone,
-    billing_account_id,
-    calls_steps_module AS steps_module,
-    calls_module_aggregation AS module_aggregation,
+    calls_ivr_id AS doc_ivr_id,
+    document_identification AS doc_document_identification,
+    document_type AS doc_document_type,
+    module_sequece AS doc_module_sequence,
+    step_sequence AS doc_step_sequence
+  FROM
+    keepcoding.ivr_detail
+  WHERE
+    document_identification NOT IN ('NULL',
+      'DESCONOCIDO') QUALIFY ROW_NUMBER() OVER(PARTITION BY SAFE_CAST(doc_ivr_id AS string)
+    ORDER BY
+      module_sequece DESC,
+      step_sequence DESC) = 1)
+
+SELECT
+  ivr_detail.calls_ivr_id AS ivr_id,
+  ivr_detail.calls_phone_number AS phone_number,
+  ivr_detail.calls_ivr_result AS ivr_result,
+  CASE
+    WHEN ivr_detail.calls_vdn_label LIKE "ATC%" THEN "FRONT"
+    WHEN ivr_detail.calls_vdn_label LIKE "TECH%" THEN "TECH"
+    WHEN ivr_detail.calls_vdn_label = "ABSORPTION" THEN "ABSORPTION"
+  ELSE
+  "RESTO"
+END
+  AS vdn_aggregation,
+  ivr_detail.calls_start_date AS start_date,
+  ivr_detail.calls_end_date AS end_date,
+  ivr_detail.calls_total_duration AS total_duration,
+  ivr_detail.calls_customer_segment AS customer_segment,
+  ivr_detail.calls_ivr_language AS ivr_language,
+  ivr_detail.document_type,
+  ivr_detail.document_identification,
+  IFNULL(MAX(NULLIF(ivr_detail.customer_phone, 'NULL')), 'DESCONOCIDO') AS customer_phone,
+  -- ivr_detail.customer_phone,
+  IFNULL(MAX(NULLIF(ivr_detail.billing_account_id, 'NULL')), 'DESCONOCIDO') AS billing_account_id,
+  -- ivr_detail.billing_account_id,
+  ivr_detail.calls_steps_module AS steps_module,
+  ivr_detail.calls_module_aggregation AS module_aggregation,
+  MAX(
   IF
-    (module_name = 'AVERIA_MASIVA',1,0) AS masiva_lg,
+    (ivr_detail.module_name = 'AVERIA_MASIVA',1,0)) AS masiva_lg,
+  MAX(
   IF
-    (step_name = 'CUSTOMERINFOBYDNI.TX'
-      AND step_description_error = NULLIF(step_description_error, "NULL"),1,0) AS info_by_dni_lg,
+    (ivr_detail.step_name = 'CUSTOMERINFOBYPHONE.TX'
+      AND ivr_detail.step_description_error = "NULL",1,0)) AS info_by_phone_lg,
+  MAX(
+  IF
+    (ivr_detail.step_name = 'CUSTOMERINFOBYDNI.TX'
+      AND ivr_detail.step_description_error = "NULL",1,0)) AS info_by_dni_lg,
     CASE
-      WHEN TIMESTAMP_DIFF(calls_start_date, LAG(calls_start_date) OVER (PARTITION BY calls_phone_number ORDER BY calls_start_date), HOUR) <= 24 THEN 1
+      WHEN TIMESTAMP_DIFF(ivr_detail.calls_start_date, LAG(ivr_detail.calls_start_date) OVER (PARTITION BY ivr_detail.calls_phone_number ORDER BY ivr_detail.calls_start_date), HOUR) <= 24 THEN 1
     ELSE
     0
   END
     AS repeated_phone_24H,
     CASE
-      WHEN COUNT(*) OVER (PARTITION BY calls_phone_number ORDER BY calls_end_date ASC ROWS BETWEEN 1 FOLLOWING AND UNBOUNDED FOLLOWING) > 0 THEN 1
+      WHEN TIMESTAMP_DIFF(ivr_detail.calls_start_date, LEAD(ivr_detail.calls_start_date) OVER (PARTITION BY ivr_detail.calls_phone_number ORDER BY ivr_detail.calls_start_date), HOUR) <= 24 THEN 1
     ELSE
     0
   END
     AS cause_recall_phone_24H
   FROM
     keepcoding.ivr_detail
-  WHERE
-    document_type <> 'NULL'
-    AND document_type <> 'DESCONOCIDO'
-    AND document_identification <> 'DESCONOCIDO'
-    AND document_identification <> 'NULL'
-    AND customer_phone <> 'DESCONOCIDO'
-    AND customer_phone <> 'NULL'
-    AND billing_account_id <> 'DESCONOCIDO'
-    AND billing_account_id <> 'NULL' QUALIFY ROW_NUMBER() OVER(PARTITION BY SAFE_CAST(calls_ivr_id AS string)) = 1 );
+  LEFT JOIN
+    doc
+  ON
+    ivr_detail.calls_ivr_id = doc.doc_ivr_id
+  LEFT JOIN
+    keepcoding.ivr_detail recalls
+  ON
+    ivr_detail.calls_phone_number <> 'NULL'
+    AND ivr_detail.calls_phone_number = recalls.calls_phone_number
+    AND ivr_detail.calls_ivr_id <> recalls.calls_ivr_id
+  GROUP BY
+    ivr_id, phone_number, ivr_result, vdn_aggregation, start_date, end_date, total_duration, customer_segment, ivr_language, steps_module, module_aggregation, document_type, document_identification;
